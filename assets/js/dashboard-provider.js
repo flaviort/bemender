dc52ba1d1aeeb7477a12d23f00de49fea309168f
Flávio Roberto Troszczanczuk <flavioczuk@gmail.com>
2020-08-17 18:08:37 +0100

    provider profile ready

--- assets/js/dashboard-provider.js
+++ assets/js/dashboard-provider.js
@@ -23,5 +23,72 @@ $( document ).ready(function() {
 	$("#edit-address .save").click(function() {
 		$("#edit-address").slideUp();
 	});
+
+	// INTRODUCTION
+	$(".profile .introduction .top-part .edit").click(function(e) {
+		e.preventDefault();
+		$(".profile .introduction .introduction-text").prop('disabled', false);
+		$(".profile .introduction .introduction-text").addClass('editable');
+		$(".profile .introduction .introduction-text").focus();
+		$.when($(this).fadeOut()).done(function() {
+			$(".profile .introduction .top-part .save-cancel").fadeIn();
+		});
+	});
+
+	$(".profile .introduction .top-part .save-cancel .save").click(function(e) {
+		e.preventDefault();
+		$(".profile .introduction .introduction-text").prop('disabled', true);
+		$(".profile .introduction .introduction-text").removeClass('editable');
+		$.when($(".profile .introduction .top-part .save-cancel").fadeOut()).done(function() {
+			$(".profile .introduction .top-part .edit").fadeIn();
+		});
+	});
+
+	$(".profile .introduction .top-part .save-cancel .cancel").click(function(e) {
+		e.preventDefault();
+		$(".profile .introduction .introduction-text").prop('disabled', true);
+		$(".profile .introduction .introduction-text").removeClass('editable');
+		$.when($(".profile .introduction .top-part .save-cancel").fadeOut()).done(function() {
+			$(".profile .introduction .top-part .edit").fadeIn();
+		});
+	});
+
+	// SERVICES BOXES
+	$(".profile .services .boxes .box .title").click(function() {
+		$(this).parent('.box').toggleClass('active');
+	});
+
+	// GALLERY BOXES - DELETE PHOTO
+	$(".profile .gallery .boxes .box .delete").click(function(e) {
+		e.preventDefault();
+		$(this).parent('.box').addClass('active');
+	});
+
+	$(".profile .gallery .boxes .box .overlay .actions button").click(function(e) {
+		e.preventDefault();
+		$(this).parents('.box').removeClass('active');
+	});
+
+	$("#gallery-done").click(function(e) {
+		e.preventDefault();
+	});
 	
 });
+
+// DROPZONE
+Dropzone.autoDiscover = false;
+var that = $("#upload-zone");
+var myDropzone = new Dropzone('#upload-zone', {
+	addRemoveLinks: true,
+	uploadMultiple: true,
+	autoProcessQueue: true,
+	acceptedFiles: "image/*, application/pdf",
+	init: function(){
+		this.on("successmultiple", function(file, data) {
+			$(that).addClass("dz-success");
+		});
+		this.on("reset", function(file) {
+			$(that).removeClass("dz-success");
+		});
+	},
+}); // END DROPZONE
